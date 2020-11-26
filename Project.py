a1071ffd67261837795782a7d00c7e57230636cc
M7Bas <74269001+M7Bas@users.noreply.github.com>
2020-11-26 22:02:21 +0300

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,6 +1,9 @@
 from tkinter import *
 import tkinter.font as font
 from PIL import ImageTk, Image
+from AI import *
+from SE import *
+from FC import *
 
 class Window(Frame):
     def __init__(self, master=None):
@@ -11,13 +14,14 @@ class Window(Frame):
         
         self.myFont = font.Font(family='Helvetica', size=18, weight='bold')
         self.CheckbuttonFont = font.Font(family='Helvetica', size=13, weight='bold')
+        self.buttonFont = font.Font(family='Helvetica', size=12, weight='bold')
         self.labelFont = font.Font(family='Helvetica', size=13, weight='bold')
 
         self.backgroundImage()
         self.setBar1()
         self.firstPage()
 
-        self.pageNum=0
+        self.pageNum = 0
 
     def setMasterUp(self):
         self.master.title("UPM Computer Major Plans")
@@ -35,9 +39,34 @@ class Window(Frame):
         imageLabel.image = bar
         imageLabel.place( x=-2, y=-2)
 
+    def setLogoImage(self, fileName,x ,y):
+        image1 = Image.open(fileName)
+        image1 = image1.resize((89,93), Image.ANTIALIAS)
+        logo = ImageTk.PhotoImage(image1)
+
+        imageLabel = Label(self, image=logo ,bg="#050627")
+        imageLabel.image = logo
+        imageLabel.place(x=x ,y=y)
+        return imageLabel
+
     def firstPage(self): # Here is the first page function
-        self.startlabel = self.label1("Press START",20,120)
-        self.startButton = self.button1("Start", self.startPages, relx = 0.5)
+        self.startlabel = self.label1("Hello! Please select a major :",20,120)
+        self.AI_Button = self.button1("Artificial intelligence", self.AI_press, relx = 0.2, rely = 0.5)
+        self.SE_Button = self.button1("Software Engineering", self.SE_press, relx = 0.81, rely = 0.5)
+        self.FC_Button = self.button1("Cyber Security", self.FC_press, relx = 0.5, rely = 0.5)
+
+        self.AI_logo = self.setLogoImage("AI.jpg", 60 , 250)
+        self.SE_logo = self.setLogoImage("SE.jpg", 375, 250)
+        self.FC_logo = self.setLogoImage("FC.jpg", 215, 250)
+
+    def AI_press(self):
+        AI_startPages(self)
+
+    def SE_press(self):
+        SE_startPages(self)
+
+    def FC_press(self):
+        FC_startPages(self)
 
     def label1(self, text, x ,y): # This label with 18 size font
         label1 = Label(self, text=f"{text}", bg="#050627", fg='white')
@@ -58,7 +87,7 @@ class Window(Frame):
     def button1(self, text, command, relx=0.8, rely = 0.9 ,anchor = CENTER):
         button1 = Button(self, text=f"{text}", command=command, relief=RIDGE, bg="#4CBB17", fg="white")
         button1.place(relx = relx, rely = rely, anchor = anchor)
-        button1["font"] = self.CheckbuttonFont
+        button1["font"] = self.buttonFont
         return button1
 
     def checkbutton1(self, text, x, y):
@@ -68,273 +97,6 @@ class Window(Frame):
         checkbutton1.place(x=x, y=y)
         checkbutton1["font"] = self.CheckbuttonFont
         return checkbutton1, var, text
-    
-    def startPages(self):
-        self.startButton.destroy()
-        self.startlabel.destroy()
-
-        self.firstPageLabel = self.label1("Check the courses you have passed",20,120)
-        self.nextButton = self.button1("Next",self.pages)
-        self.skipButton = self.button1("Skip all", self.pages, relx=0.67)
-        self.pages()
-
-    def pages(self):
-        if self.pageNum == 0:
-            self.page1()
-            self.pageNum += 1
-        
-        elif self.pageNum == 1:
-            self.destryPage1()
-            self.page2()
-            self.pageNum += 1
-        
-        elif self.pageNum == 2:
-            self.destryPage2()
-            self.page3()
-            self.pageNum += 1
-        
-        elif self.pageNum == 3:
-            self.destryPage3()
-            self.page4()
-            self.pageNum += 1
-        
-        elif self.pageNum == 4:
-            self.destryPage4()
-            self.page5()
-            self.pageNum += 1
-        
-        elif self.pageNum == 5:
-            self.destryPage5()
-            self.page6()
-            self.pageNum += 1
-        
-        elif self.pageNum == 6:
-            self.destryPage6()
-            self.page7()
-            self.pageNum += 1
-        
-        elif self.pageNum == 7:
-            self.destryPage7()
-            self.page8()
-            self.pageNum += 1
-        
-        elif self.pageNum == 8:
-            self.destryPage8()
-            self.finalPage()
-
-
-    def page1(self):
-        self.MATH101 = self.checkbutton1("MATH 101", 30, 160)
-        self.PHYS101 = self.checkbutton1("PHYS 101", 30, 185)
-        self.CS111   = self.checkbutton1("CS 111"  , 30, 210)
-        self.ENGL101 = self.checkbutton1("ENGL 101", 30, 235)
-        
-    def destryPage1(self):
-        self.MATH101[0].destroy()
-        self.PHYS101[0].destroy()
-        self.CS111[0].destroy()
-        self.ENGL101[0].destroy()
-
-    def page2(self):
-        self.MATH102 = self.checkbutton1("MATH 102", 30, 160)
-        self.PHYS102 = self.checkbutton1("PHYS 102", 30, 185)
-        self.CS112   = self.checkbutton1("CS 112"  , 30, 210)
-        self.ENGL102 = self.checkbutton1("ENGL 102", 30, 235)
-
-    def destryPage2(self):
-        self.MATH102[0].destroy()
-        self.PHYS102[0].destroy()
-        self.CS112[0].destroy()
-        self.ENGL102[0].destroy()
-
-    def page3(self):
-        self.MATH201 = self.checkbutton1("MATH 201", 30, 160)
-        self.CS321   = self.checkbutton1("CS 321", 30, 185)
-        self.CS201   = self.checkbutton1("CS 201", 30, 210)
-        self.CS211   = self.checkbutton1("CS 211", 30, 235)
-        self.CS351   = self.checkbutton1("CS 351", 30, 260)
-
-    def destryPage3(self):
-        self.MATH201[0].destroy()
-        self.CS321[0].destroy()
-        self.CS201[0].destroy()
-        self.CS211[0].destroy()
-        self.CS351[0].destroy()
-
-    def page4(self):
-        self.STAT232 = self.checkbutton1("STAT 232", 30, 160)
-        self.MATH204 = self.checkbutton1("MATH 204", 30, 185)
-        self.AI372   = self.checkbutton1("AI 372", 30, 210)
-        self.ENGL201 = self.checkbutton1("ENGL 201", 30, 235)
-        self.CS332   = self.checkbutton1("CS 332", 30, 260)
-    
-    def destryPage4(self):
-        self.STAT232[0].destroy()
-        self.MATH204[0].destroy()
-        self.AI372[0].destroy()
-        self.ENGL201[0].destroy()
-        self.CS332[0].destroy()    
-
-    def page5(self):
-        self.MATH303 = self.checkbutton1("MATH 303", 30, 160)
-        self.AI381   = self.checkbutton1("AI 381", 30, 185)
-        self.AI305   = self.checkbutton1("AI 305", 30, 210)
-        self.AI385   = self.checkbutton1("AI 385", 30, 235)
-        self.AI361   = self.checkbutton1("AI 361", 30, 260)
-
-    def destryPage5(self):
-        self.MATH303[0].destroy()
-        self.AI381[0].destroy()
-        self.AI305[0].destroy()
-        self.AI385[0].destroy()
-        self.AI361[0].destroy() 
-
-    def page6(self):
-        self.AI382 = self.checkbutton1("AI 382", 30, 160)
-        self.AI316 = self.checkbutton1("AI 316", 30, 185)
-        self.AI318 = self.checkbutton1("AI 318", 30, 210)
-        self.AI312 = self.checkbutton1("AI 312", 30, 235)
-        self.AI306 = self.checkbutton1("AI 306", 30, 260)
-
-    def destryPage6(self):
-        self.AI382[0].destroy()
-        self.AI316[0].destroy()
-        self.AI318[0].destroy()
-        self.AI312[0].destroy()
-        self.AI306[0].destroy()
-
-    def page7(self):
-        self.AI491 = self.checkbutton1("AI 491", 30, 160)
-        self.AI407 = self.checkbutton1("AI 407", 30, 185)
-        self.AI417 = self.checkbutton1("AI 417", 30, 210)
-    
-    def destryPage7(self):
-        self.AI491[0].destroy()
-        self.AI407[0].destroy()
-        self.AI417[0].destroy()
-        
-
-    def page8(self):
-        self.AI492 = self.checkbutton1("AI 492", 30, 160)
-        self.AI418 = self.checkbutton1("AI 418", 30, 185)
-        self.AI438 = self.checkbutton1("AI 438", 30, 210)
-    
-    def destryPage8(self):
-        self.AI492[0].destroy()
-        self.AI418[0].destroy()
-        self.AI438[0].destroy()
-
-    def finalPage(self):
-        self.firstPageLabel.destroy()
-        self.nextButton.destroy()
-        self.skipButton.destroy()
-
-        self.finalPageLabel = self.label1("Here are the courses that remain for you :",20,120)
-        self.quitButton1 = self.button1("Quit", quit)
-
-        coursesList = []
-
-        if self.MATH101[1].get() == 0:
-            coursesList.append(self.MATH101[2])
-        else :
-            if self.MATH102[1].get() == 0:
-                coursesList.append(self.MATH102[2])
-            else:
-                if self.MATH201[1].get() == 0:
-                    coursesList.append(self.MATH201[2])
-                if self.CS201[1].get() == 0:
-                    coursesList.append(self.CS201[2])
-                if self.STAT232[1].get() == 0:
-                    coursesList.append(self.STAT232[2])
-                if self.MATH204[1].get() == 0:
-                    coursesList.append(self.MATH204[2])
-                else:
-                    if self.AI305[1].get() == 0:
-                        coursesList.append(self.AI305[2])
-                    else:
-                        if self.AI318[1].get() == 0:
-                            coursesList.append(self.AI318[2])
-                        if self.AI312[1].get() == 0:
-                            coursesList.append(self.AI312[2])
-                        if self.AI306[1].get() == 0:
-                            coursesList.append(self.AI306[2])
-                        if self.AI417[1].get() == 0:
-                            coursesList.append(self.AI417[2])
-                    if self.AI385[1].get() == 0:
-                        coursesList.append(self.AI385[2])
-                    if self.AI361[1].get() == 0:
-                        coursesList.append(self.AI361[2])
-                if self.MATH303[1].get() == 0:
-                    coursesList.append(self.MATH303[2])
-                else:
-                    if self.AI316[1].get() == 0:
-                        coursesList.append(self.AI316[2])
-                        
-        if self.PHYS101[1].get() == 0:
-            coursesList.append(self.PHYS101[2])
-        else:
-            if self.PHYS102[1].get() == 0:
-                coursesList.append(self.PHYS102[2])
-                
-        if self.ENGL101[1].get() == 0:
-            coursesList.append(self.ENGL101[2])
-        else:
-            if self.ENGL102[1].get() == 0:
-                coursesList.append(self.ENGL102[2])
-
-        if self.CS111[1].get() == 0:
-            coursesList.append(self.CS111[2])
-        else:
-            if self.CS112[1].get() == 0:
-                coursesList.append(self.CS112[2])
-            else:
-                if self.CS321[1].get() == 0:
-                    coursesList.append(self.CS321[2])
-                if self.CS211[1].get() == 0:
-                    coursesList.append(self.CS211[2])
-                else:
-                    if self.AI381[1].get() == 0:
-                        coursesList.append(self.AI381[2])
-                    else:
-                        if self.AI382[1].get() == 0:
-                            coursesList.append(self.AI382[2])
-                        else:
-                            if self.AI438[1].get() == 0:
-                                coursesList.append(self.AI438[2])
-
-                if self.CS351[1].get() == 0:
-                    coursesList.append(self.CS351[2])
-                if self.ENGL201[1].get() == 0:
-                    coursesList.append(self.ENGL201[2])
-                if self.CS332[1].get() == 0:
-                    coursesList.append(self.CS332[2])
-                    
-
-
-        coursesList.sort()
-
-        x = 30
-        xCounter = 0
-        y = 155   
-
-        for course in coursesList:
-                self.label2(course,x,y)
-                # coursesList.append(course[2])
-                # if course
-
-                y += 25
-                xCounter += 1
-                if xCounter == 7:
-                    xCounter = 0
-                    y = 155
-                    x += 100
-
-
-        # for course in courses:
-        #     if course[1].get() == 0:
-        #         course = self.label2(f"{course[2]}",x,y)
-
-
 
 root = Tk()
 root.geometry("520x400+400+150")
