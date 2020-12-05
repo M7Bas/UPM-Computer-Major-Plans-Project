ae5e2d49355429797e9069a4d0053d187fa9419b
AmmarKhalifah <74413456+AmmarKhalifah@users.noreply.github.com>
2020-12-05 20:06:38 +0300

    Update SE.py

--- SE.py
+++ SE.py
@@ -155,9 +155,9 @@ def page2(self):
     self.ENGL102 = self.checkbutton1("ENGL 102", 30, 235)
 
 def destroyPage2(self):
-    self.MATH102[0].destroy()
     self.PHYS102[0].destroy()
     self.CS112[0].destroy()
+    self.MATH102[0].destroy()
     self.ENGL102[0].destroy()
 
 def page3(self):
@@ -189,14 +189,14 @@ def destroyPage4(self):
 def page5(self):
     self.MATH204 = self.checkbutton1("MATH 204", 30, 160)
     self.CS321   = self.checkbutton1("CS 321", 30, 185)
-    self.CS464   = self.checkbutton1("CS 464", 30, 210)
+    self.SE464   = self.checkbutton1("CS 464", 30, 210)
     self.SE311   = self.checkbutton1("SE 311", 30, 235)
     self.SE323   = self.checkbutton1("SE 323", 30, 260)
 
 def destroyPage5(self):
     self.MATH204[0].destroy()
     self.CS321[0].destroy()
-    self.CS464[0].destroy()
+    self.SE464[0].destroy()
     self.SE311[0].destroy()
     self.SE323[0].destroy() 
 
@@ -239,6 +239,95 @@ def finalPage(self):
     self.quitButton1 = self.button1("Quit", quit, relx=0.28)
 
     coursesList = []
+
+    if self.MATH101[1].get() == 0:
+        coursesList.append(self.MATH101[2])
+    else :
+        if self.MATH102[1].get() == 0:
+            coursesList.append(self.MATH102[2])
+        else :
+            if self.MATH202[1].get() == 0:
+                coursesList.append(self.MATH202[2])
+            if self.MATH204[1].get() == 0:
+                coursesList.append(self.MATH204[2])
+            if self.STAT232[1].get() == 0:
+                coursesList.append(self.STAT232[2])
+            if self.CS201[1].get() == 0:
+                coursesList.append(self.CS201[2])
+            else:
+                if self.CS224[1].get() == 0:
+                    coursesList.append(self.CS224[2])
+                if self.CS211[1].get() == 0:
+                    coursesList.append(self.CS211[2])
+                else:
+                    if self.CS321[1].get() == 0:
+                        coursesList.append(self.CS321[2])
+
+    if self.PHYS101[1].get() == 0:
+        coursesList.append(self.PHYS101[2])
+    else:
+        if self.PHYS102[1].get() == 0:
+            coursesList.append(self.PHYS102[2])
+
+    if self.CS111[1].get() == 0:
+        coursesList.append(self.CS111[2])
+    else:
+        if self.CS112[1].get() == 0:
+            coursesList.append(self.CS112[2])
+        else:
+            if self.CS351[1].get() == 0:
+                coursesList.append(self.CS351[2])
+            if self.CS211[1].get() == 0:
+                coursesList.append(self.CS211[2])
+            else:
+                if self.SE262[1].get() == 0:
+                    coursesList.append(self.SE262[2])
+                else:
+                    if self.SE464[1].get() == 0:
+                        coursesList.append(self.SE464[2])               
+                    if self.SE311[1].get() == 0:
+                        coursesList.append(self.SE311[2])
+                    else:
+                        if self.SE463[1].get() == 0:
+                            coursesList.append(self.SE463[2])
+                    if self.SE323[1].get() == 0:
+                        coursesList.append(self.SE323[2])
+                    else:
+                        if self.SE342[1].get() == 0:
+                            coursesList.append(self.SE342[2])
+                        if self.SE324[1].get() == 0:
+                            coursesList.append(self.SE324[2])
+                    if self.SE431[1].get() == 0:
+                        coursesList.append(self.SE431[2])
+
+
+
+            if self.CS332[1].get() == 0:
+                coursesList.append(self.CS332[2])
+            else:
+                if self.SE472[1].get() == 0:
+                    coursesList.append(self.SE472[2])
+    if self.ENGL101[1].get() == 0:
+        coursesList.append(self.ENGL101[2])
+    else:
+        if self.ENGL102[1].get() == 0:
+            coursesList.append(self.ENGL102[2])
+        else:
+            if self.ENGL201[1].get() ==0:
+                coursesList.append(self.ENGL201[2])
+
+                    
+
+
+
+
+
+
+
+
+
+
+
     coursesList.sort()
 
     x = 30
